4beb5af57fe2d1de74f81ac5150ffab948b19586
MayaraFernandesP <mayara.fernandes2@etec.sp.gov.br>
2024-08-12 20:14:51 -0300

    Aula1208 - Criando a Estrutura do FIrebase no Projeto em React

--- minideiablog/src/firebase/config.jsx
+++ minideiablog/src/firebase/config.jsx
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDkeRsRwl0iQsnyc_nfQ6Ba-b01-_s4yZ0",
   authDomain: "miniblog-may.firebaseapp.com",
@@ -16,6 +11,5 @@ const firebaseConfig = {
   measurementId: "G-528GGMHP43"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
