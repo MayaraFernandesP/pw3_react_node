6e3faf97e5385a8f99e668828b1067b585b2ea36
MayaraFernandesP <mayara.fernandes2@etec.sp.gov.br>
2024-09-04 20:46:55 -0300

    Aula0904 EStrutura de Projeto React

--- minideiablog/src/App.jsx
+++ minideiablog/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import Footer from './components/Footert'
+
 function App() {
   return (
     <>
       <div>
         <NavBar />
+        <Footer />
       </div>
     </>
   )
---
+++ minideiablog/src/components/Footert.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './Footer.module.css'
+
+const Footer = () => {
+  return (
+    <>
+  <footer className={style.footer}>
+    <h3>Ideias de quem gosta de ficr com o pé na estrada!</h3>
+    <p>IdeiasApp &copy; 2024</p>
+</footer>
+    </>
+  )
+}
+
+export default Footer
